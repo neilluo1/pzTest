e014db774de157075402eb765e5b1a5c30b7f1c9
Neil <cluo@panzura.com>
2020-11-24 10:41:31 +0800

    modify test

--- benchmark/cases/conftest.py
+++ benchmark/cases/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 from py._xmlgen import html
+from datetime import datetime
+import pytz
+
 
 def pytest_configure(config):
     config._metadata['Author'] = 'Neil'
@@ -7,12 +10,14 @@ def pytest_configure(config):
 
 @pytest.mark.optionalhook
 def pytest_html_results_table_header(cells):
+    cells.insert(1, html.th('Start Time'))
     cells.insert(2, html.th('Description'))
     cells.pop()
 
 
 @pytest.mark.optionalhook
 def pytest_html_results_table_row(report, cells):
+    cells.insert(1, html.td(report.start_time))
     cells.insert(2, html.td(report.description))
     cells.pop()
 
@@ -21,4 +26,5 @@ def pytest_html_results_table_row(report, cells):
 def pytest_runtest_makereport(item):
     outcome = yield
     report = outcome.get_result()
+    report.start_time = datetime.now(tz=pytz.timezone('US/Pacific'))
     report.description = str(item.function.__doc__)
