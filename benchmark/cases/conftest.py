eae7be0d1380d49a45751ec7964895509964269a
Neil <cluo@panzura.com>
2020-11-24 10:22:32 +0800

    modify test

--- benchmark/cases/conftest.py
+++ benchmark/cases/conftest.py
@@ -1,5 +1,24 @@
 import pytest
-
+from py._xmlgen import html
 
 def pytest_configure(config):
     config._metadata['Author'] = 'Neil'
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th('Description'))
+    cells.pop()
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.pop()
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
--- benchmark/cases/fs_mark.py
+++ benchmark/cases/fs_mark.py
@@ -14,41 +14,71 @@ def fs_mark_obj():
     return fs_mark_obj
 
 
-def test_1000files_1M(fs_mark_obj):
+def test_case1(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M
+    """
     fs_mark_obj.run('-s 1048576 -n 1000')
 
 
-def test_1000files_1M_noSync(fs_mark_obj):
+def test_case2(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M, NoSync
+    """
     fs_mark_obj.run('-s 1048576 -n 1000 -S 0')
 
 
-def test_1000files_1M_fsyncBeforeClose(fs_mark_obj):
+def test_case3(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M, FsyncBeforeClose
+    """
     fs_mark_obj.run('-s 1048576 -n 1000 -S 1')
 
 
-def test_1000files_1M_sync(fs_mark_obj):
+def test_case4(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M, Sync
+    """
     fs_mark_obj.run('-s 1048576 -n 1000 -S 2')
 
 
-def test_1000files_1M_postReverseFsync(fs_mark_obj):
+def test_case5(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M, PostReverseFsync
+    """
     fs_mark_obj.run('-s 1048576 -n 1000 -S 3')
 
 
-def test_1000files_1M_syncPostReverseFsync(fs_mark_obj):
+def test_case6(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M, SyncPostReverseFsync
+    """
     fs_mark_obj.run('-s 1048576 -n 1000 -S 4')
 
 
-def test_1000files_1M_postFsync(fs_mark_obj):
+def test_case7(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M, PostFsync
+    """
     fs_mark_obj.run('-s 1048576 -n 1000 -S 5')
 
 
-def test_1000files_1M_syncPostFsync(fs_mark_obj):
+def test_case8(fs_mark_obj):
+    """
+    File number: 1000, File size: 1M, SyncPostFsync
+    """
     fs_mark_obj.run('-s 1048576 -n 1000 -S 6')
 
 
-def test_5000files_1M_4threads(fs_mark_obj):
+def test_case9(fs_mark_obj):
+    """
+    File number: 5000, File size: 1M, 4threads
+    """
     fs_mark_obj.run('-s 1048576 -n 5000 -t 4')
 
 
-def test_4000files_1M_32sub_dirs(fs_mark_obj):
+def test_case10(fs_mark_obj):
+    """
+    File number: 4000, File size: 1M, 32 sub dirs
+    """
     fs_mark_obj.run('-s 1048576 -n 4000 -D 32')
