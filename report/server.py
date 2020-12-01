5bbd31f8e315976274ab304f6122f98577cab9fb
Neil <cluo@panzura.com>
2020-12-01 10:39:47 +0800

    Add iozone test

--- report/server.py
+++ report/server.py
@@ -33,5 +33,21 @@ def fs_mark_report(report_html):
     return render_template(os.path.join(report_html))
 
 
+@app.route('/iozone')
+def iozone_reports():
+    report_path = os.path.join(work_dir, 'report/templates/')
+    reports = []
+    for template in os.listdir(report_path):
+        if 'iozone-report' in template and os.path.isfile(os.path.join(report_path, template)):
+            reports.append(template)
+
+    return render_template('iozone.html', reports=reports)
+
+
+@app.route('/iozone/<report_html>')
+def iozone_report(report_html):
+    return render_template(os.path.join(report_html))
+
+
 if __name__ == '__main__':
     app.run(host=get_localhost_ip(), port=5001)
